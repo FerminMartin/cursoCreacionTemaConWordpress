61d0b0651e42c27d3484aced6ceb53a21e40e642
fermin <fermartorr01@gmail.com>
2023-03-28 07:23:05 +0200

    Listamos nuestros productos (custom post type) y los imprimimos de manera dinámica en una single page

--- functions.php
+++ functions.php
@@ -13,4 +13,82 @@ function plz_assets(){
 }
 
 add_action("wp_enqueue_scripts","plz_assets");
-?>
+
+function plz_analytics(){
+  ?>
+  <h1></h1>
+  <?php
+}
+
+add_action("wp_body_open","plz_analytics");
+
+function plz_theme_supports(){
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+  add_theme_support('custom-logo', 
+    array(
+      "width" => 170,
+      "height" => 35,
+      "flex-width" => true,
+      "flex-heigh" => true,
+    )
+    );  
+}
+
+add_action("after_setup_theme","plz_theme_supports");
+
+function plz_add_menus(){
+  register_nav_menus(
+    array(
+    'menu-principal' => "Menu principal",
+    'menu-responsive' => "Menu responsive"
+    )
+  );
+}
+
+add_action("after_setup_theme","plz_add_menus");
+
+function plz_add_sidebar() {
+  register_sidebar(
+    array(
+      'name' => 'Pie de página',
+      'id' => 'pie-pagina',
+      'before_widget' => '',
+      'after_widget' => ''
+    )
+    );
+}
+
+add_action("widgets_init","plz_add_sidebar");
+
+function plz_add_custom_post_type(){
+  $labels = array(
+    'name' => 'Producto',
+    'singular_name' => 'Producto',
+    'all_items' => 'Todos los productos',
+    'add_new' => 'Añadir producto'
+  );
+
+ $args = array(
+        'labels' => $labels,
+        'description' => 'Productos para listar en un catálogo.',
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'producto' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 5,
+        'supports'           => array( 'title', 'editor', 'author', 'thumbnail' ),
+        'taxonomies'         => array('category'),
+        'show_in_rest'       => true,
+        'menu_icon'          => 'dashicons-cart'
+    );
+
+
+  register_post_type('producto',$args);
+}
+
+add_action("init","plz_add_custom_post_type");
--- header.php
+++ header.php
@@ -1,38 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php  language_attributes(); ?>>
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo("charset") ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>platziCommerce</title>
     <?php wp_head(); ?>
 </head>
 
+
 <body>
 
+    <?php wp_body_open(); ?>
     <header class="encabezado">
         <div class="container-fluid gx-5 py-3">
             <div class="row">
                 <div class="encabezado__hamburguesa col-2">
-                    <a href="#">
-                        <img src="<?php echo get_template_directory_uri()?>/assets/img/icon-menu.svg" alt="menu icon">
-                    </a>
+                  <a href="#">
+                    <img src="<?php echo get_template_directory_uri()?>/assets/img/icon-menu.svg" alt="menu icon">
+                  </a>
                 </div>
                 <div class="enbezado__logo col-5 col-md-2 px-2 px-md-4">
-                    <a href="/">
-                        <img src="<?php echo get_template_directory_uri()?>/assets/img/logo.png" width="100%" alt="Yard Sales">
-                    </a>
+                  <?php the_custom_logo() ?> 
                 </div>
                 <div class="encabezado__menu col-6 col-md-5">
-                    <ul>
-                        <li class="encabezado__item current"><a href="#">All</a></li>
-                        <li class="encabezado__item"><a href="#">Clothes</a></li>
-                        <li class="encabezado__item"><a href="#">Electronics</a></li>
-                        <li class="encabezado__item"><a href="#">Furniture</a></li>
-                        <li class="encabezado__item"><a href="#">Toys</a></li>
-                        <li class="encabezado__item"><a href="#">Others</a></li>
-                    </ul>
+                   <?php wp_nav_menu(
+                    array(
+                      "menu" => 'menu-principal'
+                    )
+                   ); ?>
                 </div>
                 <div class="encabezado__cart col-5 col-md-1">
                     <a href="sign_in.html" class="encabezado_sign-in">
@@ -44,14 +40,11 @@
                 </div>
             </div>
             <div class="encabezado__menu-responsive">
-                <ul>
-                    <li class="encabezado__item current"><a href="#">All</a></li>
-                    <li class="encabezado__item"><a href="#">Clothes</a></li>
-                    <li class="encabezado__item"><a href="#">Electronics</a></li>
-                    <li class="encabezado__item"><a href="#">Furniture</a></li>
-                    <li class="encabezado__item"><a href="#">Toys</a></li>
-                    <li class="encabezado__item"><a href="#">Others</a></li>
-                </ul>
+              <?php wp_nav_menu(
+                    array(
+                      "menu" => 'menu-responsive'
+                    )
+                   ); ?>
             </div>
         </div>
     </header>
